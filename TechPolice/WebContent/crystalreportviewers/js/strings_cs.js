647186eee9b7dc87f2397f4a82aaa3563698fb4b
SUPRADIP DEY <97578674+DESUP2@users.noreply.github.com>
2023-01-07 11:08:16 +0530

    Add files via upload
    Extract images folder.

---
+++ TechPolice/WebContent/crystalreportviewers/js/strings_cs.js
@@ -0,0 +1,87 @@
+// LOCALIZATION STRING
+
+// Strings for calendar.js and calendar_param.js
+var L_Today     = "Dnes";
+var L_January   = "Leden";
+var L_February  = "\u00danor";
+var L_March     = "Boezen";
+var L_April     = "Duben";
+var L_May       = "Kviten";
+var L_June      = "Eerven";
+var L_July      = "Eervenec";
+var L_August    = "Srpen";
+var L_September = "Z\u00e1o\u00ed";
+var L_October   = "O\u00edjen";
+var L_November  = "Listopad";
+var L_December  = "Prosinec";
+var L_Su        = "Ne";
+var L_Mo        = "Po";
+var L_Tu        = "\u00dat";
+var L_We        = "St";
+var L_Th        = "Et";
+var L_Fr        = "P\u00e1";
+var L_Sa        = "So";
+
+// strings for dt_param.js
+var L_TIME_SEPARATOR = ":";
+var L_AM_DESIGNATOR = "dop.";
+var L_PM_DESIGNATOR = "odp.";
+
+// strings for range parameter
+var L_FROM = "Od {0}";
+var L_TO = "Do {0}";
+var L_AFTER = "Po {0}";
+var L_BEFORE = "Poed {0}";
+var L_FROM_TO = "Od {0} do {1}";
+var L_FROM_BEFORE = "Od {0} do do\u00edve ne\u017e {1}";
+var L_AFTER_TO = "Po {0} a do {1}";
+var L_AFTER_BEFORE = "Po {0} a poed {1}";
+
+// Strings for prompts.js and prompts_param.js
+var L_BadNumber     = "Tento parametr je typu \"E\u00edslo\" a mu\u017ee obsahovat pouze z\u00e1porn\u00e9 znam\u00e9nko, e\u00edslice (\"0-9\"), symboly pro skupiny e\u00edslic a desetinn\u00e9 znam\u00e9nko. Opravte zadanou hodnotu parametru.";
+var L_BadCurrency   = "Tento parametr je typu \"Mina\" a mu\u017ee obsahovat pouze z\u00e1porn\u00e9 znam\u00e9nko, e\u00edslice (\"0-9\"), symboly pro skupiny e\u00edslic a desetinn\u00e9 znam\u00e9nko. Opravte zadanou hodnotu parametru.";
+var L_BadDate       = "Tento parametr je typu \"Datum\" a mus\u00ed b\u00fdt ve form\u00e1tu \"Date(rrrr,mm,dd)\", kde \"rrrr\" je rok vyj\u00e1doen\u00fd etyomi e\u00edslicemi, \"mm\" je mis\u00edc (napo. leden = 1) a \"dd\" je pooad\u00ed dne v dan\u00e9m mis\u00edci.";
+var L_BadDateTime   = "Tento parametr je typu \"Datum-eas\" a spr\u00e1vn\u00fd form\u00e1t je \"DateTime(rrrr,mm,dd,hh,mm,ss)\". \"rrrr\" je rok vyj\u00e1doen\u00fd etyomi e\u00edslicemi, \"mm\" je mis\u00edc (napo. leden = 1), \"dd\" je den v mis\u00edci, \"hh\" jsou hodiny (24hodinov\u00fd form\u00e1t), \"mm\" jsou minuty a \"ss\" sekundy.";
+var L_BadTime       = "Tento parametr je typu \"Eas\" a mus\u00ed b\u00fdt ve form\u00e1tu \"Time(hh,mm,ss)\", kde \"hh\" jsou hodiny ve 24hodinov\u00e9m form\u00e1tu, \"mm\" jsou minuty a \"ss\" sekundy.";
+var L_NoValue       = "Bez hodnoty";
+var L_BadValue      = "Pro nastaven\u00ed hodnoty \"Bez hodnoty\" mus\u00edte na hodnotu \"Bez hodnoty\" nastavit hodnoty Od i Do.";
+var L_BadBound      = "Nelze soueasni nastavit hodnoty \"Bez spodn\u00ed hranice\" a \"Bez horn\u00ed hodnoty\".";
+var L_NoValueAlready = "Tento parametr ji\u017e m\u00e1 nastavenu hodnotu \"Bez hodnoty\". Ne\u017e budete poid\u00e1vat dal\u0161\u00ed hodnoty, odeberte hodnotu \"Bez hodnoty\"";
+var L_RangeError    = "Poe\u00e1tek rozsahu nesm\u00ed b\u00fdt vit\u0161\u00ed ne\u017e konec.";
+var L_NoDateEntered = "Mus\u00edte zadat datum.";
+
+// Strings for ../html/crystalexportdialog.htm
+var L_ExportOptions     = "Mo\u017enosti exportu";
+var L_PrintOptions      = "Mo\u017enosti tisku";
+var L_PrintPageTitle    = "Vytisknout sestavu";
+var L_ExportPageTitle   = "Exportovat sestavu";
+var L_OK                = "OK";
+var L_Cancel            = "Storno";
+var L_PrintPageRange    = "Zadejte rozsah str\u00e1nek pro tisk.";
+var L_ExportPageRange   = "Zadejte rozsah str\u00e1nek pro export.";
+var L_InvalidPageRange  = "Hodnoty rozsahu str\u00e1nek jsou neplatn\u00e9.  Zadejte platn\u00fd rozsah str\u00e1nek.";
+var L_ExportFormat      = "Vyberte v seznamu form\u00e1t exportu.";
+var L_Formats           = "Form\u00e1t souboru:";
+var L_PageRange         = "Rozsah str\u00e1nek:";
+var L_All               = "V\u0161echny";
+var L_Pages             = "Str\u00e1nky:";
+var L_From              = "Od:";
+var L_To                = "Do:";
+var L_PrintStep0        = "Poi tisku:";
+var L_PrintStep1        = "1.  V po\u00ed\u0161t\u00edm zobrazen\u00e9m dialogov\u00e9m okni zvolte mo\u017enost \"Otevo\u00edt tento soubor\" a klepnite na tlae\u00edtko OK.";
+var L_PrintStep2        = "2.  Klepnite na ikonu tisk\u00e1rny v nab\u00eddce aplikace Acrobat Reader (nikoli na tlae\u00edtko tisku v prohl\u00ed\u017eeei).";
+var L_RTFFormat         = "Rich Text Format (RTF)";
+var L_AcrobatFormat     = "Adobe Acrobat (PDF)";
+var L_CrystalRptFormat  = "Crystal Reports (RPT)";
+var L_WordFormat        = "Microsoft Word (RTF)";
+var L_ExcelFormat       = "Microsoft Excel 97-2000 (XLS)";
+var L_ExcelRecordFormat = "Microsoft Excel 97-2000 - pouze data (XLS)";
+var L_EditableRTFFormat = "Microsoft Word - upraviteln\u00fd (RTF)";
+
+// Strings for print.js
+var L_PrintControlInstallError = "Instalace ovl\u00e1dac\u00edho prvku ActiveX pro tisk se nezdaoila.  K\u00f3d chyby: ";
+var L_PrintControlPlugin = "Doplnik ovl\u00e1dac\u00edho prvku Crystal Reports pro tisk";
+
+// Strings for previewerror.js
+var L_SessionExpired = "Skoneila doba platnosti relace";
+var L_PleaseRefresh = "Stisknite tlae\u00edtko Obnovit a opakujte akci";
